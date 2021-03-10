33bea1928176a59636924dc865277d488dbadcd0
shivam1534 <shivam.mkp@gmail.com>
2021-03-10 13:34:58 +0530

    Cewa Backend

--- db/index.js
+++ db/index.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://beast1534:dEoXOk37HDjzhvQ6@cluster0.kellf.mongodb.net/ecommercesr?retryWrites=true&w=majority",
+    "mongodb+srv://beast1534:dEoXOk37HDjzhvQ6@cluster0.kellf.mongodb.net/cewangov1?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }
   )
-  .then(() => console.log("Database for E-commerce is connected"))
+  .then(() => console.log("Database for CEWA NGO v1.0 is connected"))
   .catch((err) => {
     console.error("Connection Error", err.message);
   });
